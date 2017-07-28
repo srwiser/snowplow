d046c32f541e0567b5946f864ad808e7223973e5
Ben Fradet <benjamin.fradet@gmail.com>
2017-07-28 15:41:19 +0100

    Stream Enrich: add json4s dependency (closes #3348)

--- 3-enrich/stream-enrich/project/SnowplowKinesisEnrichBuild.scala
+++ 3-enrich/stream-enrich/project/SnowplowKinesisEnrichBuild.scala
@@ -37,6 +37,7 @@ object SnowplowStreamEnrichBuild extends Build {
         Libraries.argot,
         Libraries.scalazon,
         Libraries.scalaz7,
+        Libraries.json4sJackson,
         Libraries.snowplowRawEvent,
         Libraries.snowplowCommonEnrich,
         Libraries.igluClient,
